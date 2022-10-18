f57b0cf490534dab54f732ad2fb759a3fc7ed2a8
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-10-17 20:02:20 -0400

    scaffolding

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <div className='wallet-button-container'>
-        <button>Connect Wallet</button>
+        <button className='connect-btn'>Connect Wallet</button>
       </div>
-      <div>
+      <div className='minting-container-main'>
         <h1>Mint The Sante Token</h1>
-        <div>
-          <div>
+        <img alt="main page logo" src="null" />
+        <div className='minting-container-inputs'>
+          <div >
+            <p>Current Tokens Minted: 0</p>
+            <p>Max Supply: 1000000</p>
             <input placeholder='amount to mint' />
+          </div>
+          <div className='mint-btn-div'>
+
             <button>Mint Now</button>
           </div>
         </div>
       </div>
+      <div className='footer-container'>
+
       <footer>Created By Crypted Sante</footer>
+      </div>
     </div>
   );
 }
